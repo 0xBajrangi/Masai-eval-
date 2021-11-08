3fb5a722a671cf7344f393b59d2348aadeee4d9d
Pawan Janglu Lode <pawanjlode@gmail.com>
2021-11-08 16:01:12 +0530

    4

---
+++ eval 8 11/food app/navbar.js
@@ -0,0 +1,11 @@
+ function navbar() {
+
+        return `<div class="navbar">
+      <div><a href="menu.html">Menu</a></div>
+      <div><a href="login.html">Login</a></div>
+      <div><a href="signup.html">signup</a></div>
+      <div><a href="cart.html">Cart</a></div>
+       
+    </div>`;
+    }
+    export {navbar} ;
